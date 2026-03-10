a8725f452790c1ac749e3d76862d3c944698e435
Antonella <212781898+antodollparts@users.noreply.github.com>
2026-03-09 21:01:47 -0500

    feat: Add responsive  navigation, section, cookie banner, and footer

---
+++ js/script.js
@@ -0,0 +1,6 @@
+const hamburguer = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburguer.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
